3056e05de055990ecc6c72b634c0cf0ea806fcc0
mhalonso96 <celo.alonso@gmail.com>
2023-06-19 22:24:21 -0300

    ✅ refresh project

--- ponto_turistico/models.py
+++ ponto_turistico/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
+from localizacoes.models import Localizacao
 
 class PontoTuristico(models.Model):
     name = models.CharField(max_length= 255)
@@ -10,6 +11,7 @@ class PontoTuristico(models.Model):
     attraction = models.ManyToManyField(Atracao)
     comment = models.ManyToManyField(Comentario)
     review = models.ManyToManyField(Avaliacao)
+    placement = models.ForeignKey(Localizacao, on_delete= models.CASCADE)
 
     def __str__(self):
         return self.name
